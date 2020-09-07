9e0fe1980160cf14ba8ad734f7423743fd86cbad
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-07 19:39:18 +0530

    state change

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -5,12 +5,28 @@ class Calculator extends React.Component {
     a: 10,
     b: 5,
   };
-  add = this.state.a + this.state.b;
+  setA = (e) => {
+    this.setState({
+      a: parseInt(e.target.value),
+    });
+  };
+  setB = (e) => {
+    this.setState({
+      b: parseInt(e.target.value),
+    });
+  };
   render() {
     const substraction = this.state.a - this.state.b;
+    const add = this.state.a + this.state.b;
     return (
       <div>
-        {"Addition : " + this.add}
+        A : <input className="inputA" onChange={this.setA} />
+        <br /> <br />
+        B : <input className="inputB" onChange={this.setB} />
+        <br /> <br />
+        {"A : " + this.state.a + " " + "B : " + this.state.b}
+        <br /> <br />
+        {"Addition : " + add}
         <br />
         {"Substraction : " + substraction}
         <br />
