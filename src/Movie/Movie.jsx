e9853072ce47fcbce31b787669e898bc18be9a3e
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-07 09:54:19 +0530

    creating movie-card and applting styles

--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -1,9 +1,11 @@
 import * as React from "react";
+import "./style.css";
 
 function Movie() {
   return (
-    <div>
-      <h1>Movie</h1>
+    <div className="movie-card">
+      <img src="https://m.media-amazon.com/images/M/MV5BZWJmMGFiZGItZTU3My00NTZjLWI2NjItNzAzNDM3NmNlNjVhXkEyXkFqcGdeQXVyOTEwNTIwNDQ@._V1_.jpg" />
+      <label>123.78k Maharshi</label>
     </div>
   );
 }
