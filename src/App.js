f7a225a2cccd2312d84067e471bafcaf4edcbd61
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-07 11:37:46 +0530

    adding properties

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World!!!!</h1>
-      <Movie />
+      <Movie
+        name="Maharsi"
+        likes="1234.5k"
+        hero="Prince"
+        banner="https://m.media-amazon.com/images/M/MV5BZWJmMGFiZGItZTU3My00NTZjLWI2NjItNzAzNDM3NmNlNjVhXkEyXkFqcGdeQXVyOTEwNTIwNDQ@._V1_.jpg"
+      />
     </div>
   );
 }
--- src/Movie/Movie.jsx
+++ src/Movie/Movie.jsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import "./style.css";
 
-function Movie() {
+function Movie(props) {
   return (
     <div className="movie-card">
-      <img src="https://m.media-amazon.com/images/M/MV5BZWJmMGFiZGItZTU3My00NTZjLWI2NjItNzAzNDM3NmNlNjVhXkEyXkFqcGdeQXVyOTEwNTIwNDQ@._V1_.jpg" />
-      <label>123.78k Maharshi</label>
+      <img src={props.banner} />
+      <label>
+        {props.name} {props.hero} {props.likes}
+      </label>
     </div>
   );
 }
