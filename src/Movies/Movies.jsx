29e944e6477b3a07b84256941be787a48842ec75
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-07 17:27:33 +0530

    creating a ES6 function and calling it

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -3,25 +3,30 @@ import Movie from "../Movie/Movie";
 import "./styles.css";
 
 class Movies extends React.Component {
+  getMovie = (name, likes, banner) => {
+    return <Movie name={name} likes={likes} banner={banner} />;
+  };
+
   render() {
     return (
       <div className="movies-section">
-        <Movie
-          name="Maharsi"
-          likes="1234.5k"
-          banner="https://m.media-amazon.com/images/M/MV5BZWJmMGFiZGItZTU3My00NTZjLWI2NjItNzAzNDM3NmNlNjVhXkEyXkFqcGdeQXVyOTEwNTIwNDQ@._V1_.jpg"
-        />
+        {this.getMovie(
+          "Maharsi",
+          "1234.5k",
+          "https://m.media-amazon.com/images/M/MV5BZWJmMGFiZGItZTU3My00NTZjLWI2NjItNzAzNDM3NmNlNjVhXkEyXkFqcGdeQXVyOTEwNTIwNDQ@._V1_.jpg"
+        )}
 
-        <Movie
-          name="Sahoo"
-          likes="5283.7k"
-          banner="https://i.pinimg.com/474x/c2/56/5d/c2565df2e1e36bc00150166f1e9caa33.jpg"
-        />
-        <Movie
-          name="Jersey"
-          likes="3583.7k"
-          banner="https://i.pinimg.com/736x/2b/9f/e4/2b9fe45f7ad9752821b868c3f5410df9.jpg"
-        />
+        {this.getMovie(
+          "Sahoo",
+          "5283.7k",
+          "https://i.pinimg.com/474x/c2/56/5d/c2565df2e1e36bc00150166f1e9caa33.jpg"
+        )}
+
+        {this.getMovie(
+          "Jersey",
+          "3583.2k",
+          "https://i.pinimg.com/736x/2b/9f/e4/2b9fe45f7ad9752821b868c3f5410df9.jpg"
+        )}
       </div>
     );
   }
