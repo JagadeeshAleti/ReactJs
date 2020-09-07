7e1c1f4ce8490a4f4b8a788585779e53040cda32
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-07 17:56:28 +0530

    Explaining state using calculator

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -5,8 +5,20 @@ class Calculator extends React.Component {
     a: 10,
     b: 5,
   };
+  add = this.state.a + this.state.b;
   render() {
-    return <div>{this.state.a + " and " + this.state.b}</div>;
+    const substraction = this.state.a - this.state.b;
+    return (
+      <div>
+        {"Addition : " + this.add}
+        <br />
+        {"Substraction : " + substraction}
+        <br />
+        {"Multiplication : " + this.state.a * this.state.b}
+        <br />
+        {"Divison : " + this.state.a / this.state.b}
+      </div>
+    );
   }
 }
 
