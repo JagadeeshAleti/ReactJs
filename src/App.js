0489b0edee6b1d361900d39e7b596898fba6f5f3
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-07 09:34:49 +0530

    Creating simple movie component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Movie from "./Movie/Movie";
 function App() {
   return (
     <div className="App">
       <h1>Hello World!!!!</h1>
+      <Movie />
     </div>
   );
 }
