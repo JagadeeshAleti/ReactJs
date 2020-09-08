09f11bce43c8c04e20d002069ec6eeea45564c8a
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-08 10:10:09 +0530

    condional opertaing on Calci

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -4,35 +4,52 @@ class Calculator extends React.Component {
   state = {
     a: 10,
     b: 5,
+    showResult: false,
   };
   setA = (e) => {
     this.setState({
       a: parseInt(e.target.value),
+      showResult: false,
     });
   };
   setB = (e) => {
     this.setState({
       b: parseInt(e.target.value),
+      showResult: false,
+    });
+  };
+  setResultToTrue = (e) => {
+    this.setState({
+      showResult: true,
     });
   };
   render() {
-    const substraction = this.state.a - this.state.b;
-    const add = this.state.a + this.state.b;
     return (
       <div>
+        {"Default value of A : " + this.state.a}
+        <br /> <br />
+        {"Default value of B : " + this.state.b}
+        <br /> <br />
         A : <input className="inputA" onChange={this.setA} />
         <br /> <br />
         B : <input className="inputB" onChange={this.setB} />
         <br /> <br />
-        {"A : " + this.state.a + " " + "B : " + this.state.b}
+        <button onClick={this.setResultToTrue}>Result</button>
         <br /> <br />
-        {"Addition : " + add}
-        <br />
-        {"Substraction : " + substraction}
-        <br />
-        {"Multiplication : " + this.state.a * this.state.b}
-        <br />
-        {"Divison : " + this.state.a / this.state.b}
+        {this.state.showResult ? (
+          <div>
+            <br />
+            {"Addition : " + parseInt(this.state.a + this.state.b)}
+            <br />
+            {"Substraction : " + parseInt(this.state.a - this.state.b)}
+            <br />
+            {"Multiplication : " + this.state.a * this.state.b}
+            <br />
+            {"Divison : " + this.state.a / this.state.b}
+          </div>
+        ) : (
+          <div>Click on Result</div>
+        )}
       </div>
     );
   }
