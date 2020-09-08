aca34c8fa08e37ed0b9a501ea1c665ea19f2ecf2
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-09-08 18:38:25 +0530

    Add simple route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Movies from "./Movies/Movies";
+import Calculator from "./Calculator/Calculator";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Movies />
+      <Router>
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/calci" component={Calculator}></Route>
+      </Router>
     </div>
   );
 }
